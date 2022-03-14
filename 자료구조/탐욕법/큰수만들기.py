73943da7606bd697d47d5f254c4df591790f1a62
sion <aabbcc030421@gmail.com>
2022-03-14 23:15:55 +0900

    2022.03.14

---
+++ 자료구조/탐욕법/큰수만들기.py
@@ -0,0 +1,9 @@
+def solution(number, k):
+    answer = [] # Stack
+    
+    for num in number:
+        while k > 0 and answer and answer[-1] < num:
+            answer.pop()
+            k -= 1
+        answer.append(num)
+        
