2ff21f0042afa6269f14d7a22c582a84e02c6023
sion <aabbcc030421@gmail.com>
2022-04-04 23:13:25 +0900

    2022..04.04

---
+++ 자료구조/다이나믹 프로그래밍/파일 합치기.py
@@ -0,0 +1,17 @@
+import math
+ 
+def solve():
+  n = int(input())
+  arr = [int(x) for x in input().split()]
+  rst = [[0 for _ in range(n)] for _ in range(n)]
+  for j in range(1, n):
+    for i in range(j-1, -1, -1):
+      small = math.inf
+      for k in range(j-i):
+        small = min(small, rst[i][i+k] + rst[i+k+1][j])
+      rst[i][j] = small + sum(arr[i:j+1])
+  print(rst[0][n-1])
+ 
+t = int(input())
+for _ in range(t):
+  solve()
